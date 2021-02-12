5ee8ed5ea41dee804efa620c20fd41e447a18e36
YuchenW123 <71779762+YuchenW123@users.noreply.github.com>
2021-02-12 13:19:01 +0800

    Revert "preview.1"
    This reverts commit ed8e63051f43a449b4695d71838f3cf75e1b115a.

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,9 +0,0 @@
-
-require('./assets/Script/FastForward');
-require('./assets/Script/Game');
-require('./assets/Script/MoodBar');
-require('./assets/Script/Schedule');
-require('./assets/Script/ScheduleBar');
-require('./assets/Script/Task');
-require('./assets/Script/TaskItem');
-require('./assets/Script/TaskItemAreaLayout');
